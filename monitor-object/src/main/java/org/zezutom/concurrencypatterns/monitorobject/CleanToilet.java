8def2b0e1d972c4b0453854b5d570fbc211f610a
zezutom <zezulatomas@gmail.com>
2014-08-23 21:33:49 +0200

    Code clean-up and a README

--- monitor-object/src/main/java/org/zezutom/concurrencypatterns/monitorobject/CleanToilet.java
+++ monitor-object/src/main/java/org/zezutom/concurrencypatterns/monitorobject/CleanToilet.java
@@ -9,10 +9,13 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class CleanToilet implements Toilet {
 
+    // Monitor Lock used by the synchronized methods
     private final ReentrantLock lock;
 
+    // Monitor Condition - the toilet can only be used by a single person at a time
     private Condition oneAtATimeCondition;
 
+    // The guarded object's state - the 'volatile' flag is crucial for the signaling to work
     private volatile int counter;
 
     public CleanToilet() {
@@ -24,12 +27,12 @@ public class CleanToilet implements Toilet {
     public boolean enter() {
         lock.lock();
         try {
-            while (counter > 0) {
+            while (counter > 0) {   // wait while the toilet is being used
                 oneAtATimeCondition.awaitUninterruptibly();
             }
 
             if (++counter == 1) {
-                oneAtATimeCondition.signal();
+                oneAtATimeCondition.signal();   // the toilet has been successfully acquired
             }
 
             return isOccupied();
@@ -43,13 +46,13 @@ public class CleanToilet implements Toilet {
         lock.lock();
         try {
 
-            while (counter <= 0) {
+            while (counter <= 0) {  // leaving a vacant toilet makes no sense
                 oneAtATimeCondition.awaitUninterruptibly();
             }
 
             if(isOccupied()) {
                 if (--counter == 0) {
-                    oneAtATimeCondition.signal();
+                    oneAtATimeCondition.signal();   // the toilet is free to use from this point on
                 }
             }
         } finally {
