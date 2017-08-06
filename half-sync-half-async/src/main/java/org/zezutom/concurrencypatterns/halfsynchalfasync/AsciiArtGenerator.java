ec637b2ef896914717f365428504ff900161f2b5
zezutom <zezulatomas@gmail.com>
2017-08-06 14:12:39 +0200

    Improved file handling, accepts an absolute path to both the input and output file.

--- half-sync-half-async/src/main/java/org/zezutom/concurrencypatterns/halfsynchalfasync/AsciiArtGenerator.java
+++ half-sync-half-async/src/main/java/org/zezutom/concurrencypatterns/halfsynchalfasync/AsciiArtGenerator.java
@@ -8,6 +8,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Credit goes to https://evilzone.org/java/(java-code)image-to-ascii-art-generator/
@@ -17,100 +21,103 @@ import java.net.URISyntaxException;
  */
 public class AsciiArtGenerator {
 
-    public static final double RED = 0.30;
+  public static final double RED = 0.30;
 
-    public static final double GREEN = 0.11;
+  public static final double GREEN = 0.11;
 
-    public static final double BLUE = 0.59;
+  public static final double BLUE = 0.59;
 
-    private BufferedImage img;
+  private BufferedImage img;
 
-    double pixval;
+  double pixval;
 
-    private PrintWriter printWriter;
+  private PrintWriter printWriter;
 
-    private FileWriter fileWriter;
+  private FileWriter fileWriter;
 
-    private int[] gradients = new int[] {240, 210, 190, 170, 120, 110, 80, 60};
+  private int[] gradients = new int[]{240, 210, 190, 170, 120, 110, 80, 60};
 
-    private char[] chars = new char[] {' ', '.', '*', '+', '^', '&', '8', '#', '@'};
+  private char[] chars = new char[]{' ', '.', '*', '+', '^', '&', '8', '#', '@'};
 
-    /**
-     * Converts an image to its ASCII representation.
-     *
-     * @param imgPath   path to the image, relative to /src/main/resources
-     * @param outPath   path to the resulting text file, relative to ./data
-     * @return true, if the conversion succeeds, false otherwise
-     */
-    public boolean convertToAscii(String imgPath, String outPath) {
-        try {
-            printWriter = new PrintWriter(fileWriter = new FileWriter(getDataDir(outPath)));
-            img = ImageIO.read(getResource(imgPath));
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException("The image couldn't be created: " + e);
+  /**
+   * Converts an image to its ASCII representation.
+   *
+   * @param imgPath path to the image, relative to /src/main/resources
+   * @param outPath path to the resulting text file, relative to ./data
+   * @return true, if the conversion succeeds, false otherwise
+   */
+  public boolean convertToAscii(String imgPath, String outPath) {
+    try {
+      printWriter = new PrintWriter(fileWriter = new FileWriter(outPath));
+      Path filePath = Paths.get(imgPath);
+      if (Files.exists(filePath)) {
+        img = ImageIO.read(filePath.toFile());
+      } else {
+        File resource = getResource(imgPath);
+        if (resource != null && resource.isFile()) {
+          img = ImageIO.read(resource);
+        } else {
+          throw new IllegalArgumentException(String.format("File '%s' couldn't be found. Does it exist?", imgPath));
         }
-
-
-        for (int i = 0; i < img.getHeight(); i++)
-        {
-            for (int j = 0; j < img.getWidth(); j++)
-            {
-                Color pixcol = new Color(img.getRGB(j, i));
-                pixval = (((pixcol.getRed() * RED) + (pixcol.getBlue() * BLUE) + (pixcol.getGreen() * GREEN)));
-                print(px2Char(pixval));
-            }
-            try {
-                printWriter.println("");
-                printWriter.flush();
-                fileWriter.flush();
-            } catch (Exception e) {
-                throw new RuntimeException("The awesome ASCII art couldn't be saved: " + e);
-            }
-        }
-        return true;
-    }
-
-    private String getDataDir(String filename) {
-        return System.getProperty("user.dir") + "/half-sync-half-async/data/" + filename;
-    }
-
-    private File getResource(String filename) throws URISyntaxException {
-        return new File(getClass().getResource("/" + filename).toURI());
+      }
+    } catch (IOException | URISyntaxException e) {
+      throw new RuntimeException("The image couldn't be created: " + e);
     }
 
 
-    public char px2Char(double px)
-    {
-        Character c = null;
+    for (int i = 0; i < img.getHeight(); i++) {
+      for (int j = 0; j < img.getWidth(); j++) {
+        Color pixcol = new Color(img.getRGB(j, i));
+        pixval = (((pixcol.getRed() * RED) + (pixcol.getBlue() * BLUE) + (pixcol.getGreen() * GREEN)));
+        print(px2Char(pixval));
+      }
+      try {
+        printWriter.println("");
+        printWriter.flush();
+        fileWriter.flush();
+      } catch (Exception e) {
+        throw new RuntimeException("The awesome ASCII art couldn't be saved: " + e);
+      }
+    }
+    return true;
+  }
 
-        for (int i = 0; i < gradients.length; i++) {
-            if (px >= gradients[i]) {
-                c = chars[i];
-                break;
-            }
-        }
+  private File getResource(String filename) throws URISyntaxException {
+    URL resource = getClass().getResource("/" + filename);
+    return (resource == null) ? null : new File(resource.toURI());
+  }
 
-        if (c == null) {
-            c = chars[chars.length - 1];
-        }
-        return c;
+
+  public char px2Char(double px) {
+    Character c = null;
+
+    for (int i = 0; i < gradients.length; i++) {
+      if (px >= gradients[i]) {
+        c = chars[i];
+        break;
+      }
     }
 
-
-    public void print(char c)
-    {
-        try {
-            printWriter.print(c);
-            printWriter.flush();
-            fileWriter.flush();
-        } catch (Exception e) {
-            throw new RuntimeException("Something went wrong when saving to disc: " + e);
-        }
+    if (c == null) {
+      c = chars[chars.length - 1];
     }
+    return c;
+  }
 
-    public static void main(String[] args) {
-        AsciiArtGenerator art = new AsciiArtGenerator();
-        art.convertToAscii("audrey_hepburn.jpg", "audrey.txt");
+
+  public void print(char c) {
+    try {
+      printWriter.print(c);
+      printWriter.flush();
+      fileWriter.flush();
+    } catch (Exception e) {
+      throw new RuntimeException("Something went wrong when saving to disc: " + e);
     }
+  }
+
+  public static void main(String[] args) {
+    AsciiArtGenerator art = new AsciiArtGenerator();
+    art.convertToAscii("audrey_hepburn.jpg", "audrey.txt");
+  }
 
 }
--- half-sync-half-async/src/main/java/org/zezutom/concurrencypatterns/halfsynchalfasync/BlockingDispatcher.java
+++ half-sync-half-async/src/main/java/org/zezutom/concurrencypatterns/halfsynchalfasync/BlockingDispatcher.java
@@ -8,12 +8,20 @@ package org.zezutom.concurrencypatterns.halfsynchalfasync;
  */
 public class BlockingDispatcher {
 
+    /**
+     * Takes an image (jpg, png) and converts it to an ASCII representation.
+     *
+     * @param imgPath This takes either an absolute path to an image (JPG, PNG)
+     *                or a relative path to a classpath resources (src/main/resources)
+     * @param outPath Path to the output txt file
+     * @return  True if the conversion has been successful, false otherwise.
+     */
     public boolean convertToAscii(String imgPath, String outPath) {
         return new AsciiArtGenerator().convertToAscii(imgPath, outPath);
     }
 
     public static void main(String[] args) {
-        boolean result = new BlockingDispatcher().convertToAscii("audrey_hepburn01.jpeg", "audrey.txt");
+        boolean result = new BlockingDispatcher().convertToAscii("audrey_hepburn.jpg", "half-sync-half-async/data/audrey.txt");
         System.out.println("RESULT: " + result);
     }
 }
